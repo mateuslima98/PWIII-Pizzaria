061067ed5c1595a1ffa0b61b2bb5c2d0c18f31d9
mateus-bea <mateus_lima98@hotmail.com>
2019-10-04 21:37:41 -0300

    exercicio_6

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'has-comanda',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComandaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
 
   ngOnInit() {
   }
