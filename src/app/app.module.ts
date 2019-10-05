d11126baa9d5e19e9184aa9d90d21f1348fee16e
mateus-bea <mateus_lima98@hotmail.com>
2019-10-04 21:10:50 -0300

    exercicio_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormBuilder, FormGroup} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComandaComponent } from './comanda/comanda.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
