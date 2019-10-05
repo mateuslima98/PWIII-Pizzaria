db83eff38826ae7c927dc48ea035ef114b06ebb9
mateus-bea <mateus_lima98@hotmail.com>
2019-10-04 22:47:18 -0300

    final_ex

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -17,8 +17,28 @@ export class ComandaComponent implements OnInit {
       pizzas : this.formBuilder.control(''),
       recheios : this.formBuilder.control(''),
       pessoas : this.formBuilder.control(''),
-      taxa :this.formBuilder.control('true')
+      taxa :this.formBuilder.control(false)
       });
   }
 
+  onProcessar(){
+    let chopp=this.FormComanda.value.chopp;
+    let pizzas=this.FormComanda.value.pizzas;
+    let recheios=this.FormComanda.value.recheios;
+    let pessoas=this.FormComanda.value.pessoas;
+    let taxa=this.FormComanda.value.taxa;
+
+    let totalConsumo =  (chopp *7.30) + (pizzas * 31.50) + (recheios * 5.00);
+    let servicos = totalConsumo * 0.10;
+    let total = totalConsumo;
+
+    if(taxa){
+      total = totalConsumo + servicos;
+    }
+
+    let consumo_pessoa = totalConsumo / pessoas;
+
+    
+    alert(`Consumo: R$ ${totalConsumo.toFixed(2)} \n Serviços: R$ ${servicos.toFixed(2)} \n Total: R$ ${total.toFixed(2)} \n Por Pessoa: R$ ${consumo_pessoa.toFixed(2)}`);
+  }
 }
